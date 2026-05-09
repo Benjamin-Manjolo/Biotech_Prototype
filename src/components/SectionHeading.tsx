16196d48395548a8eef589b3f31f28b1881e1aea
Benjamin-Manjolo <benjaminmanjolo39@gmail.com>
2026-05-09 23:24:35 +0200

    first commit

---
+++ src/components/SectionHeading.tsx
@@ -0,0 +1,9 @@
+export const SectionHeading = () => (
+  <div className="mb-6 md:mb-8">
+    <h2 className="text-2xl md:text-4xl font-bold leading-tight">
+      <span className="text-gray-400">One Business.</span>
+      <br />
+      <span className="text-gray-900">One Payment Solution.</span>
+    </h2>
+  </div>
+);
